d7a5fd02a94429b12e6e3dc8393bd27b91eb8dfa
Filip Taifas <andreifilip123@gmail.com>
2019-09-25 20:32:19 +0300

    Implement basic search and load more functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import RecipeSearch from './RecipeSearch';
+import RecipeList from './RecipeList';
+import secrets from './secrets';
+
+const App = () => {
+    const [recipeQuery, setRecipeQuery] = useState('chicken');
+    const [recipes, setRecipes] = useState([]);
+    const [fromTo, setFromTo] = useState(0);
+
+    const handleRecipeQueryChange = (e) => setRecipeQuery(e.target.value);
+
+    const composeUrl = () => {
+        const baseUrl = 'https://api.edamam.com/search';
+        const query = `q=${recipeQuery}`;
+        const appId = `app_id=${secrets.appId}`;
+        const appKey = `app_key=${secrets.appKey}`;
+        const from = `from=${fromTo}`;
+        return `${baseUrl}?${query}&${appId}&${appKey}&${from}`;
+    }
+
+    const searchRecipes = () => {
+        const url = composeUrl();
+        fetch(url)
+            .then(response => {
+                if(response.status !== 200) {
+                    console.warn('Api request failed. There might be a problem with your api key. Did you store it in secrets.json ?');
+                    return;
+                }
+
+                response.json().then(data => {
+                    if (data.hits) {
+                        setRecipes(prevRecipes => [...prevRecipes, ...data.hits]);
+                        setFromTo(prevFromTo => prevFromTo + 10);
+                    }
+                })
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+        <RecipeSearch onChange={handleRecipeQueryChange} onClick={searchRecipes}/>
+        <RecipeList recipes={recipes}/>
+        {recipes.length !== 0 && (<button className="button" onClick={searchRecipes}>Load more recipes...</button>)}
     </div>
   );
 }
